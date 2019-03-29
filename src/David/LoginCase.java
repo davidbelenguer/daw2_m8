a0d05e2c1e7c3d3430cfc34d81e23b687b4abf01
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-03-29 16:19:14 +0100

    LoginCase.java: Added function close connection to BBDD

--- src/David/LoginCase.java
+++ src/David/LoginCase.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginCase {
 
 	public static boolean insertUser;
 
-	/*CREATE TABLE*/
+	/* CREATE TABLE */
 	public static boolean insertUser(String uName, String uPass, String uEmail) {
 		Connection con;
 		con = ConnectionDB.getConnection();
@@ -17,28 +18,27 @@ public class LoginCase {
 		conectionNick = ConnectionDB.getConnection();
 		PreparedStatement pss = null;
 		PreparedStatement nick = null;
-		
-		
+
 		try {
 			String consultaNick = "select * from users where nick=?;";
 			nick = conectionNick.prepareStatement(consultaNick);
-			nick.setString(1,uName);
+			nick.setString(1, uName);
 			ResultSet Respuesta = nick.executeQuery();
-			
+
 			if (Respuesta.next()) {
 				return false;
-			}else {
-				
+			} else {
+
 				String consulta = "insert into users (NICK,PASS,EMAIL) VALUES (?,?,?);";
 				pss = con.prepareStatement(consulta);
-				pss.setString(1,uName);
-				pss.setString(2,uPass);
-				pss.setString(3,uEmail);
+				pss.setString(1, uName);
+				pss.setString(2, uPass);
+				pss.setString(3, uEmail);
 				con.commit();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				if (pss != null) {
 					pss.close();
@@ -46,7 +46,46 @@ public class LoginCase {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+			closeAll(nick, pss);
 		}
 		return false;
 	}
+
+	private static void closeAll(PreparedStatement nick, PreparedStatement pss) {
+		if (nick != null) {
+			try {
+				nick.close();
+			} catch (Exception e) {
+			}
+		}
+		if (pss != null) {
+			try {
+				pss.close();
+			} catch (Exception e) {
+				
+			}
+		}
+
+	}
+
+//	private void closeAll(ResultSet rs, Statement st, Connection cnx) {
+//		if (rs != null) {
+//			try {
+//				rs.close();
+//			} catch (Exception ex) {
+//			}
+//		}
+//		if (st != null) {
+//			try {
+//				st.close();
+//			} catch (Exception ex) {
+//			}
+//		}
+//		if (cnx != null) {
+//			try {
+//				cnx.close();
+//			} catch (Exception ex) {
+//			}
+//		}
+//	}
 }
