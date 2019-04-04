6f9749fa96bc14b812ae6ee8353a76e1122f62f2
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-04-04 19:54:46 +0200

    ConnectionDB.java: Chenged err for LOGGER

--- src/David/ConnectionDB.java
+++ src/David/ConnectionDB.java
@@ -3,18 +3,22 @@ package David;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConnectionDB {
 
 	static String DRIVER="org.hsqldb.jdbcDriver";
 	static String URL="jdbc:hsqldb:file:C:\\Users\\super\\eclipse-workspace\\M08\\WebContent\\WEB-INF\\lib\\srv_db.tmp";
 	public static Connection getConnection() {
+	    final Logger LOGGER = Logger.getLogger("David");
+
 		Connection conec = null;
 		try {
 			Class.forName(DRIVER);
 			conec=DriverManager.getConnection(URL, "sa","");
 		} catch (ClassNotFoundException | SQLException e) {
-			System.out.println("Error"+e);
+			LOGGER.log(Level.SEVERE,e.toString());
 		}
 		return conec;
 	}
