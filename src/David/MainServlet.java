4054b0bc65983c4962e02071f75cdb7f51774c55
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-04-04 20:17:54 +0200

    MainServlet.java: Deleted comments

--- src/David/MainServlet.java
+++ src/David/MainServlet.java
@@ -29,12 +29,7 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.err.println("***************\n\n*********************OOOOOOOOOOOOOOOOOOOOOOOOO");
-
 		getServletContext().getRequestDispatcher("/html/index.jsp").forward(request, response);
-		
-		
 	}
 
 	/**
