b6f78fb456f17703d6a3f57b9c9c1c61a9f12f42
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-04-05 16:04:26 +0200

    prueba.java:Deleted comments

--- src/David/prueba.java
+++ src/David/prueba.java
@@ -32,7 +32,7 @@ public class prueba extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 		String email = request.getParameter("email");
 		Pattern pat = Pattern
 				.compile("^[A-Za-z0-9]+([.|-][A-za-z0-9]+)*@[A-Za-z0-9]+([.|-][A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
