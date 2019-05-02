98a414966b95117069e7e172d1c3d0a44ae80020
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-05-02 20:16:35 +0200

    validacion.java: deleted comments

--- src/David/validacion.java
+++ src/David/validacion.java
@@ -32,14 +32,13 @@ public class validacion extends HttpServlet {
     public validacion() {
         super();
         
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		final Logger LOGGER = Logger.getLogger("David");
 		
 		try {
@@ -99,7 +98,7 @@ public class validacion extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
